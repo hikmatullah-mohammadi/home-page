670339cd497e7648a233895a4f251d98c236696a
Hikmatullah Mohammadi <70486343+hikmatullah-mohammadi@users.noreply.github.com>
2021-12-29 00:29:30 +0430

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-
+var time_elem = document.getElementById("time");
+var dat_elem = document.getElementById("date");
+let updat_time = function () {
+  time_elem.innerHTML = new Date().toLocaleTimeString();
+  dat_elem.innerHTML = new Date().toDateString();
+};
+setInterval(updat_time, 100);
